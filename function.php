20a419a21efe0a60b9a07d4b6957bfd8ed27a1e7
Purple-Manue <purple.aformac@gmail.com>
2018-03-01 16:55:11 +0100

    img ok et début text

--- function.php
+++ function.php
@@ -4,13 +4,19 @@ include('login.php');
 	function mySqli() {
 		try {
 
-    		$bdd = new PDO('mysql:host=localhost;dbname=BDD_BLOG;charset=utf8', getUser(), getMdp());
-    		return $bdd;
+			$bdd = new PDO('mysql:host=localhost;dbname=BDD_BLOG;charset=utf8', getUser(), getMdp());
+			return $bdd;
 		}
 		catch(Exception $e){
 
-        	die('Erreur : '.$e->getMessage());
+	    	die('Erreur : '.$e->getMessage());
 		}
 	}
 
+/*
+$textht = $_POST['textht'];
+echo $textht;
+$textbas = $_POST['textbas'];
+echo $textbas;
+*/
 ?>
