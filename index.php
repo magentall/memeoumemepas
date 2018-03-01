cb35ffa9ef7445f6534fd948b53d002dccca6014
Chris Gold <chris.ratsimba87@gmail.com>
2018-03-01 10:27:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <?php include 'includes/header.html' ?>
+            <title>MemeouMemepas</title>
+
+        </head>
+
+        <body>
+          <?php include 'includes/base_js.html' ?>
+        </body>
+    </html>
