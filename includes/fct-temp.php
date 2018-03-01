80ed824a38a2fdbdc97c2a1177f5a6ef82837e16
magentall <magentallteller@gmail.com>
2018-03-01 15:24:04 +0100

    sza1.5

--- includes/fct-temp.php
+++ includes/fct-temp.php
@@ -1,11 +1,11 @@
 <?php
-
+// creation d'une url html return url.html
 function create_url($cnx){
   $rand=rand(0,700000000);
   $url_meme=$rand.".html";
   return $url_meme;
 }
-
+// vérif si url dans la base return une url valide
 function crea_check_url($cnx){
   $query="SELECT URL_mm FROM IMG_rdr";
   $resURL_MEME=mysqli_query($cnx,$query);
@@ -30,7 +30,7 @@ function crea_check_url($cnx){
     return $url_meme;
   }
 }
-
+// vérif si url dans la base return une url valide plus rapide
 function crea_check_url_v2($cnx){
   $query="SELECT URL_mm FROM IMG_rdr";
   $resURL_MEME=mysqli_query($cnx,$query);
@@ -48,7 +48,7 @@ function crea_check_url_v2($cnx){
 }
 
 ///////// CREA MULTI URL //////////////
-
+// creation d'une url
 function create_url_mul($cnx,$type,$nomfic){
   if (in_array($type, array("jpg", "jpeg", "png"))) {
     $rand=rand(0,700000000);
@@ -62,7 +62,7 @@ function create_url_mul($cnx,$type,$nomfic){
   }
 
 }
-
+// vérif si url dans la base return une url valide
 function crea_check_url($cnx,$type,$nomfic,$SELECT,$FROM){
   $query="SELECT ".$SELECT." FROM ".$FROM;
   $resURL_MEME=mysqli_query($cnx,$query);
