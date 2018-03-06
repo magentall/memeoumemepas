aa145f1961a8b8cb57667755c0cd83c1c53a297f
Purple-Manue <purple.aformac@gmail.com>
2018-03-06 16:33:23 +0100

    début foncyions php récup meme

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
+<link rel="stylesheet" href="css/style.css">
